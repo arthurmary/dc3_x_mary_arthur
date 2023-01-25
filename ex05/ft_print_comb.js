0cd7b3ab2b9583377d762b95c7109d717ee60150
arthurmary <113183213+arthurmary@users.noreply.github.com>
2023-01-25 13:57:38 +0100

    ex05_finish

---
+++ ex05/ft_print_comb.js
@@ -0,0 +1,13 @@
+function PrintCombinaison  () {
+    for (let digital = 0; digital<= 7; digital++){
+        for (let campus =0; campus<= 8; campus++){
+            for (let montpellier =0; montpellier<= 9; montpellier++){
+
+                console.log (digital,campus,montpellier);
+
+            }
+        }
+    }
+    
+}
+PrintCombinaison();
