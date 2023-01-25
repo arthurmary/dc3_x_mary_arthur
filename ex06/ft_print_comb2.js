20b77877be3e511268c650abc17b16a1cdcb0bc0
arthurmary <113183213+arthurmary@users.noreply.github.com>
2023-01-25 14:04:08 +0100

    crea_ex06

---
+++ ex06/ft_print_comb2.js
@@ -0,0 +1,12 @@
+function PrintCombinaison2  () {
+    for (let digital = 0; digital<= 8; digital++){
+        for (let campus =0; campus<= 9; campus++){
+            
+
+                console.log (digital,campus);
+  
+        }
+    }
+    
+}
+PrintCombinaison2();
